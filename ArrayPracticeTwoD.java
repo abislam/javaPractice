25dd9d005ffd5c72d2fce7aa4f36465a1dae79dd
Abed Islam <aislam360@gmail.com>
2019-10-23 23:20:21 -0400

    Gets both user inputs and updates gameboard correctly

--- ArrayPracticeTwoD.java
+++ ArrayPracticeTwoD.java
@@ -1,19 +1,21 @@
 //this comment is testing branch updates
 //branch update: correct board placement based on user input
+//add second user input
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class ArrayPracticeTwoD {
 
 	public static void main(String []args){
-		Scanner scan = new Scanner(System.in);
+		Scanner scan1 = new Scanner(System.in);
+		Scanner scan2 = new Scanner(System.in);
 		int user1, user2;
 
 
 		String[][] arr = new String[6][7];
 
 
-
+		//This code block prints the initial board
 		for(int i = 0; i < arr.length; i++){
 			for(int j = 0; j < arr[i].length; j++){
 				arr[i][j] = "*";
@@ -21,39 +23,39 @@ public class ArrayPracticeTwoD {
 			}
 			System.out.println("");
 		}
-		
+		//--------------------------------------------
+
+		//loop for getting user input and printing new board
 		while(true){
 			System.out.println("Input value for user1: ");
-			user1 = scan.nextInt() - 1;
-			while(true){
+			user1 = scan1.nextInt() - 1;
+			//while(true){
 
-
-				for(int i = 5; i >= 0; i--){
-					if(arr[i][user1] == "*"){
-						arr[i][user1] = "X";
-						break;
-					}else{
-						continue;
-					}
-				}
-				/*
-				if(arr[5][user1] == "*"){
-					arr[5][user1] = "X";
-				}else if(arr[4][user1] == "*"){
-					arr[4][user1] = "X";
-				}else if(arr[3][user1] == "*"){
-					arr[3][user1] = "X";
-				}else if(arr[2][user1] == "*"){
-					arr[2][user1] = "X";
-				}else if(arr[1][user1] == "*"){
-					arr[1][user1] = "X";
-				}else if(arr[0][user1] == "*"){
-					arr[0][user1] = "X";
-				}else{
+			for(int i = 5; i >= 0; i--){
+				if(arr[i][user1] == "*"){
+					arr[i][user1] = "X";
 					break;
-				}*/
-				break;
+				}else{
+					continue;
+				}
 			}
+			System.out.println(Arrays.deepToString(arr).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+
+			
+			System.out.println("Input value for user2: ");
+			user2 = scan2.nextInt() - 1;
+
+			for(int i = 5; i >= 0; i--){
+				if(arr[i][user2] == "*"){
+					arr[i][user2] = "O";
+					break;
+				}else{
+					continue;
+				}
+			}			
+
+				//break;
+			//}
 			
 			System.out.println(Arrays.deepToString(arr).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
 		}
