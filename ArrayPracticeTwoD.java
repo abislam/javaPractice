00cfa2a045594a08a338448a1a7d9d2e7ea278ef
Abed Islam <aislam360@gmail.com>
2019-10-23 23:45:30 -0400

    added comments for win condition algorithmn

--- ArrayPracticeTwoD.java
+++ ArrayPracticeTwoD.java
@@ -1,14 +1,14 @@
 //this comment is testing branch updates
 //branch update: correct board placement based on user input
 //branch update: add second user input
+//check for win condition
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class ArrayPracticeTwoD {
 
 	public static void main(String []args){
-		Scanner scan1 = new Scanner(System.in);
-		Scanner scan2 = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		int user1, user2;
 
 
@@ -28,8 +28,7 @@ public class ArrayPracticeTwoD {
 		//loop for getting user input and printing new board
 		while(true){
 			System.out.println("Input value for user1: ");
-			user1 = scan1.nextInt() - 1;
-			//while(true){
+			user1 = scan.nextInt() - 1;
 
 			for(int i = 5; i >= 0; i--){
 				if(arr[i][user1] == "*"){
@@ -43,7 +42,7 @@ public class ArrayPracticeTwoD {
 
 
 			System.out.println("Input value for user2: ");
-			user2 = scan2.nextInt() - 1;
+			user2 = scan.nextInt() - 1;
 
 			for(int i = 5; i >= 0; i--){
 				if(arr[i][user2] == "*"){
@@ -53,51 +52,9 @@ public class ArrayPracticeTwoD {
 					continue;
 				}
 			}			
-
-				//break;
-			//}
 			
 			System.out.println(Arrays.deepToString(arr).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
 		}
-
-		
-
-		/*
-		System.out.println("Input value for user1: ");
-		user1 = scan.nextInt();
-
-		
-		//****************************************************
-		for(int i = 0; i < arr.length; i++){
-			while(arr[][user1] == "*"){
-
-			}
-		}
-		*/
-
-		
-		/*
-		for(int i = 0; i < arr.length; i++){
-			for(int j = 0; j < arr[i].length; j++){
-				arr[i][j] = "*";
-				System.out.print(arr[i][j] + " ");
-			}
-			System.out.println("");
-		}
-		*/
-		//****************************************************
-
-
-
-		/*
-		for(int i = 0; i < arr.length; i++){
-			if(arr[i][user1] == "*"){
-				continue;
-			}else{
-				arr[i][user1-1] = "x";
-			}
-		}
-		*/
 
 	}
 }
